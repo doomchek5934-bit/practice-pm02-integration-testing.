f762a9a37e22f9db68009e18d17ac17725981847
doomchek5934-bit <doomchek5934@gmail.com>
2026-04-18 00:54:43 +1100

    Update calculator.py

--- module_A/calculator.py
+++ module_A/calculator.py
@@ -1,4 +1,4 @@
 def calculate_price(quantity, unit_price):
     if quantity < 0 or unit_price < 0:
         raise ValueError("Values cannot be negative")
-    return quantity * unit_price
+    return quantity + unit_price
